5f509feb8affb3c99c2979e711ef269e50cb0fb9
ArekStasko <arek.stasko@gmail.com>
2021-06-19 11:39:05 +0200

    add console

--- controllers/thingControllers.js
+++ controllers/thingControllers.js
@@ -5,13 +5,13 @@ exports.getThings = async(req, res) => {
     const { category } = req.params
     const id = req.query.userID
     const things = await Thing.find({category: category, author: id})
-    console.log(category, id, req.body),
     res.send({things})
  }
 
 exports.createThing = async(req, res) => {
     const thing = new Thing(req.body)
     thing.author = req.query.userID
+    console.log(req.body)
     await thing.save()
     res.send(thing)
 }
